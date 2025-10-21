2c2aa4ea8c29e28abbd60d5cb713df8efe6d6318
Anfsity <186920685+anfsity@users.noreply.github.com>
2025-10-21 14:23:42 +0800

    Add Dsu

---
+++ include/atl/graph/Dsu.hpp
@@ -0,0 +1,27 @@
+#include "DsuDef.hpp"
+
+template <typename T> size_t cp::graph::Dsu<T>::Dsu::find(size_t x) {
+  if (father[x] != x) {
+    father[x] = find(father[x]);
+  }
+  return x;
+}
+
+template <typename T> void cp::graph::Dsu<T>::Dsu::unite(size_t x, size_t y) {
+  x = find(x), y = find(y);
+  if (x == y)
+    return;
+  if (size[x] > size[y]) {
+    std::swap(x, y);
+  }
+  father[x] = y;
+  size[y] = size[y] + size[x];
+}
+
+template <typename T> bool cp::graph::Dsu<T>::Dsu::same(size_t x, size_t y) {
+  return find(x) == find(y);
+}
+
+template <typename T> size_t cp::graph::Dsu<T>::Dsu::getSize(size_t pos) {
+  return size[pos];
+}
---
+++ include/atl/graph/DsuDef.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <numeric>
+#include <vector>
+
+namespace cp {
+
+namespace graph {
+
+template <typename T>
+class Dsu {
+private:
+    std::vector<T> father, size;
+    size_t find (size_t x);
+public:
+    Dsu(size_t n = 0) :
+    father(n, 0), size(n, 0) { std::iota(size.begin(), size.end(), 0); }
+
+    void unite(size_t x, size_t y);
+    bool same(size_t x, size_t y);
+    size_t getSize(size_t pos);
+};
+
+} // namespace graph
+
+} // namespace cp
