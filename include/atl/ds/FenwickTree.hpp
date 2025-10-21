8a7b18f0ee2fdff3db3d2bd3e7121ab3dad288d7
Anfsity <186920685+anfsity@users.noreply.github.com>
2025-10-21 14:24:26 +0800

    Enhance fenwick tree

--- include/atl/ds/FenwickTree.hpp
+++ include/atl/ds/FenwickTree.hpp
@@ -1,11 +1,8 @@
 #pragma once
 #include "FenwickTreeDef.hpp"
 
-namespace cp {
-namespace ds {
-
 template <typename T>
-void FenwickTree<T>::FenwickTree::build(const std::vector<T> &other) {
+void cp::ds::FenwickTree<T>::FenwickTree::build(const std::vector<T> &other) {
   tree = other;
   for (int i = 1; i < ssize(tree); ++i) {
     int id = i + lowbit(i);
@@ -16,7 +13,7 @@ void FenwickTree<T>::FenwickTree::build(const std::vector<T> &other) {
 }
 
 template <typename T>
-void FenwickTree<T>::FenwickTree::insert(int pos, T weight) {
+void cp::ds::FenwickTree<T>::FenwickTree::insert(int pos, T weight) {
     while(pos < ssize(tree)) {
         tree[pos] += weight;
         pos += lowbit(pos);
@@ -24,7 +21,7 @@ void FenwickTree<T>::FenwickTree::insert(int pos, T weight) {
 }
 
 template <typename T>
-T FenwickTree<T>::FenwickTree::sum(int pos) {
+T cp::ds::FenwickTree<T>::FenwickTree::sum(int pos) {
     T res{};
     while (pos > 0) {
         res += tree[pos];
@@ -34,9 +31,6 @@ T FenwickTree<T>::FenwickTree::sum(int pos) {
 }
 
 template <typename T>
-T FenwickTree<T>::FenwickTree::rangeSum(int l, int r) {
+T cp::ds::FenwickTree<T>::FenwickTree::rangeSum(int l, int r) {
     return sum(r) - sum(l - 1);
 }
-
-} // namespace ds
-} // namespace cp
